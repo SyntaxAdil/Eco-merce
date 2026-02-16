18b9dffd8fd603b65169aeca6a9ddf91c9318364
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-02-16 19:51:27 +0600

    Notification badge added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "lucide-react";
-
+import { useCart } from './../context/AddToCart';
 
 const Navbar = () => {
+  const {cart}=useCart();
   return (
-    <nav className="container mx-auto max-w-6xl flex items-center justify-between my-4 px-6 py-2  rounded-full fixed top-0 left-0 right-0  z-50 backdrop-blur-2xl border border-white/10 shadow-lg ">
+    <nav className="container mx-auto max-w-6xl flex items-center justify-between my-4 px-6 py-2  rounded-full fixed top-0 left-0 right-0  z-50 backdrop-blur-2xl border border-white/10 shadow-lgnpm ">
       <div>
         <Link to={"/"}>
           <h2 className="text-xl font-bold uppercase">
             {" "}
-             Eco <span className="text-blue-700">Merce</span>
+            Eco <span className="text-blue-700">Merce</span>
           </h2>
         </Link>
       </div>
@@ -19,10 +20,14 @@ const Navbar = () => {
           <Link to={"/"}>Products</Link>
         </li>
       </ul>
-      <button className="cursor-pointer">
-        <Link to={"/carts"}>  
-        <ShoppingCart />
+      <button className="cursor-pointer relative">
+        <Link to={"/carts"}>
+          <ShoppingCart />
         </Link>
+
+        {cart.length > 0 && <span className="absolute  top-0 -right-2 w-3 h-3 p-2 rounded-full bg-red-400 flex items-center justify-center text-[12px]">{cart.length}
+
+        </span>}
       </button>
     </nav>
   );
